7809fa99830d1a92d7f5200ae69df200be9ebbff
guo-uzu <alejandro@uzu.digital>
2024-05-05 22:29:04 -0600

    test token

--- src/middlewares/validateToken.js
+++ src/middlewares/validateToken.js
@@ -3,6 +3,8 @@ import { SECRET_KEY } from "../config.js"
 
 export const authRequired = (req, res, next) => {
     const {token} = req.cookies
+    console.log(token)
+    console.log(req.cookies)
     if(!token) { 
         return res.status(401).json({
             message: "No token, autorización denegada"
